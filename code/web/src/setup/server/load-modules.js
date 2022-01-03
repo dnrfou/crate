7b6f45c7fd9231c14d86fcc329e4806b0ae273a0
dnrfou <dnrfou2018@gmail.com>
2022-01-03 19:52:21 +0900

    static

--- code/web/src/setup/server/load-modules.js
+++ code/web/src/setup/server/load-modules.js
@@ -14,6 +14,7 @@ export default function (app) {
   // Request body parser
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({extended: false}))
+  app.use(express.static('public'))
 
   // Request body cookie parser
   app.use(cookieParser())
